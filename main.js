05a7a85cfb2b1def968426f14392c32b037c5a35
Lu Frazão <lugfrazao@gmail.com>
2023-12-06 20:12:00 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#reset-button').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoNovaImg = $('#endereco-nova-img').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoNovaImg}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-img-link">
+                <a href="${enderecoNovaImg}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(2000);
+        $('#endereco-nova-img').val('');
+    })
+})
